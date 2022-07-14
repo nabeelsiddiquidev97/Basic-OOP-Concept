25e03d8bc5ec16f76f19f488084eba1c7fd63f56
Nabeel Ahmed Siddiqui <101038092+nabeelsiddiquidev97@users.noreply.github.com>
2022-07-14 15:34:23 +0500

    Add files via upload
    A program that generates the following pattern:
    
    0
    12
    345
    6789
    
    The program uses a nested for loop to get the desired pattern.

---
+++ NabsPattern.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author Nabeel Ahmed Siddiqui - https://github.com/nabeelsiddiquidev97
+ */
+
+public class NabsPattern {
+    public static void main(String[] args) {
+    int a=0, row,col;
+    for(row=1;row<=4;row++){
+    for(col=1;col<=row;col++){
+        System.out.print(a);
+        a++;
+    }    System.out.println(); 
+    }
+    }  
+}
